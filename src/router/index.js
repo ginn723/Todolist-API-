1d73c0f2170366440945451b627afebcb6cc6842
GinnLee <ginn0723@icloud.com>
2025-09-08 16:10:43 +0800

    Update configuration for TodolistAPI repository

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,11 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-
   routes: [
-    { path: '/todolist', name: 'todolist', component: TodoListView },
-    { path: '/login', name: 'login', component: LoginView },
+    { path: '/', redirect: '/register' },
     { path: '/register', name: 'register', component: RegisterView },
+    { path: '/login', name: 'login', component: LoginView },
+    { path: '/todolist', name: 'todolist', component: TodoListView },
   ],
 });
 
